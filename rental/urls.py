1494a9f9d7174b18bac634e06c54abe8939140c2
ashithaganesan <143989077+ashithaganesan@users.noreply.github.com>
2024-07-08 12:33:33 +0400

    Update urls.py

--- rental/urls.py
+++ rental/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view(), name='home'),
+    path('home/', views.HomeView.as_view(), name='home'),
     path('add', views.CreateBookView.as_view(), name='create'),
     path('home/<slug:slug>', views.DetailedBookView.as_view(), name='read'),
     path('edit/<int:id>', views.editBook, name='update'),
